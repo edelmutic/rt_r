e501c01e0e4f439209e0608f188a93300e29a135
edelmutic <edelmutic@gmail.com>
2021-04-29 15:22:06 +0300

    update files

---
+++ src/components/UserList.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import { useTypedSelector } from './../hooks/useTypedSelector';
+
+const UserList: FC = () => {
+  const { users, error, loading } = useTypedSelector((state) => state.user);
+
+  return <div></div>;
+};
+
+export default UserList;
