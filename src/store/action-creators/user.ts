17860113124b30b76d1c96f1be54820c22f140db
edelmutic <edelmutic@gmail.com>
2021-04-29 15:57:27 +0300

    update store with actions

---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { UserActionTypes, UserAction } from '../../types/user';
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      dispatch({ type: UserActionTypes.FETCH_USERS });
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users?_limit=5'
+      );
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_SUCCESS,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_ERROR,
+        payload: 'Error during fetching',
+      });
+    }
+  };
+};
